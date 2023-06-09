be62ac6b5273b421908d0c86f9743a6dfceb7575
MIDHUNCVgithub999888 <122215889+MIDHUNCVgithub999888@users.noreply.github.com>
2023-06-09 16:31:07 +0530

    Update settings.py

--- CargoManagement/settings.py
+++ CargoManagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+(5vlp4ltcyng=9ra3pntaua^h3x9@d15sh8n*@+305jzy9^-!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
